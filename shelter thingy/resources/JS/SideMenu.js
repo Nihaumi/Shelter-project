96a0a57ed34466751ce2e7d278c3a4dcfd922676
Nathalie Senechal <ns@move-elevator.de>
2020-02-06 16:33:30 +0100

    Mobile Navigation

--- shelter thingy/resources/JS/SideMenu.js
+++ shelter thingy/resources/JS/SideMenu.js
@@ -1,7 +1,64 @@
+var slideTime = 1000
+
+function hideAll() {
+    jQuery(".Layers").hide("slide", {direction: "left"}, slideTime);
+    jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
+    jQuery(".MenuBtn").show();
+
+}
+
+/*hide/hwow Nav*/
+function slideNavIn() {
+    jQuery("#navbarSupportedContent1").show("slide", {direction: "right"}, slideTime);
+}
+
+function slideNavAway() {
+    jQuery("#navbarSupportedContent1").hide("slide", {direction: "left"}, slideTime);
+}
+
+/*sliding snd transitions*/
 function openNav() {
-    document.getElementById("menu").style.width = "100px";
+
+    slideNavIn();
+    jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .8)'}, slideTime);
+    jQuery(".MenuBtn").hide();
+    console.log('Hello');
 }
 
 function closeNav() {
-    document.getElementById("menu").style.width = "0";
+
+    slideNavAway();
+    setTimeout(function () {
+        jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
+    }, slideTime / 2);
+    setTimeout(function () {
+        jQuery(".MenuBtn").show();
+
+    }, slideTime);
 }
+
+/*second Layer Our Work*/
+function openOurWorkSecondLayer() {
+    jQuery("#navbarSupportedContent1").hide();
+    jQuery(".OurWorkSecondLayer").show("slide", {direction: "right"}, slideTime);
+
+}
+
+function closeOurWorkSecondLayer() {
+    jQuery(".OurWorkSecondLayer").hide();
+    slideNavIn();
+}
+
+/*THIRD LAYER Farm Animals*/
+function openFarmAnimalsThirdLayer() {
+    jQuery(".OurWorkSecondLayer").hide();
+    jQuery(".FarmAnimalsThirdLayer").show("slide", {direction: "right"}, slideTime);
+
+}
+
+function closeFarmAnimalsThirdLayer() {
+    jQuery(".FarmAnimalsThirdLayer").hide();
+    jQuery(".OurWorkSecondLayer").show("slide", {direction: "right"}, slideTime);
+    ;
+}
+
