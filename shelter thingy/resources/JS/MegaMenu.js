29dd7af6896550afe3e2ff18216db231d4b022dd
Nathalie Senechal <ns@move-elevator.de>
2020-02-19 13:25:38 +0100

    added Searchbar

--- shelter thingy/resources/JS/MegaMenu.js
+++ shelter thingy/resources/JS/MegaMenu.js
@@ -1,6 +1,6 @@
 
 /*DeskNav*/
-
+/*
 $(document).ready(function () {
     if ($(".NavHeight").backgroundColor === 'rgba(0,0,0, 0.5)') {
         $(".NavHeight").mouseover(function () {
@@ -12,7 +12,7 @@ $(document).ready(function () {
         $(".NavHeight").animate({backgroundColor: 'rgba(0, 0, 0,.5)'});
     });
 });
-
+*/
 
 var count = 0;
 $(document).ready(function () {
@@ -29,21 +29,24 @@ function colorChange() {
     console.log(count);
 //ausklappen
     if (count == 1 % 2) {
+        //jQuery(".mega-menu").show("slide", {direction: "up"}, slideTime);
+
         jQuery(".mega-menu").animate({backgroundColor: 'rgba(0, 0, 0, .8)'}, slideTime / 2);
-        /*     setTimeout(function () {
+           setTimeout(function () {
                  jQuery(".NavHeight").animate({backgroundColor: 'rgba(0, 0, 0, .8)'}, slideTime / 2);
-             }, slideTime / 2);*/
+             }, slideTime / 2);
         //  jQuery(".mega-menu").animate({opacity: '1'}, slideTime/2 );
-        console.log('zero' + count);
+
 
     }
     //einklappen
     if (count == 0 % 2) {
+      //  jQuery(".mega-menu").hide("slide", {direction: "up"}, slideTime);
         jQuery(".mega-menu").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
-        /*  setTimeout(function () {
+        setTimeout(function () {
               jQuery(".NavHeight").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
-          }, slideTime / 2);*/
+          }, slideTime / 2);
         //  jQuery(".mega-menu").animate({opacity: '0'}, slideTime/2 );
-        console.log('ones' + count);
+
     }
 }
--- shelter thingy/resources/JS/SideMenu.js
+++ shelter thingy/resources/JS/SideMenu.js
@@ -1,35 +1,36 @@
 var slideTime = 1000;
 
 function hideAll() {
-    jQuery(".Layers").hide("slide", {direction: "left"}, slideTime);
-    jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
+    jQuery(".MobileNav").hide("slide", {direction: "left"}, slideTime);
+    jQuery(".NavColor").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
     jQuery(".MenuBtn").show();
 
 }
 
 /*hide/hwow Nav*/
 function slideNavIn() {
-    jQuery("#navbarSupportedContent1").show("slide", {direction: "right"}, slideTime);
+    jQuery("#navbarSupportedContent1").show("slide", {direction: "left"}, slideTime);
+    console.log('now')
 }
 
 function slideNavAway() {
-    jQuery("#navbarSupportedContent1").hide("slide", {direction: "left"}, slideTime);
+    jQuery("#navbarSupportedContent1").hide("slide", {direction: "right"}, slideTime);
 }
 
 /*sliding snd transitions*/
 function openNav() {
 
     slideNavIn();
-    jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .8)'}, slideTime);
+    jQuery(".NavColor").animate({backgroundColor: 'rgba(0, 0, 0, .8)'}, slideTime);
     jQuery(".MenuBtn").hide();
-    console.log('Hello');
+    console.log('mehp');
 }
 
 function closeNav() {
 
     slideNavAway();
     setTimeout(function () {
-        jQuery(".navbar").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
+        jQuery(".NavColor").animate({backgroundColor: 'rgba(0, 0, 0, .5)'}, slideTime / 2);
     }, slideTime / 2);
     setTimeout(function () {
         jQuery(".MenuBtn").show();
@@ -39,26 +40,26 @@ function closeNav() {
 
 /*second Layer Our Work*/
 function openOurWorkSecondLayer() {
-    jQuery("#navbarSupportedContent1").hide();
-    jQuery(".OurWorkSecondLayer").show("slide", {direction: "right"}, slideTime);
+    jQuery(".FirstLayer").hide();
+    jQuery(".OurWorkSecondLayer").show("slide", {direction: "left"}, slideTime);
 
 }
 
 function closeOurWorkSecondLayer() {
     jQuery(".OurWorkSecondLayer").hide();
-    slideNavIn();
+    jQuery(".FirstLayer").show("slide", {direction: "right"}, slideTime);
 }
 
 /*THIRD LAYER Farm Animals*/
 function openFarmAnimalsThirdLayer() {
     jQuery(".OurWorkSecondLayer").hide();
-    jQuery(".FarmAnimalsThirdLayer").show("slide", {direction: "right"}, slideTime);
+    jQuery(".FarmAnimalsThirdLayer").show("slide", {direction: "left"}, slideTime);
 
 }
 
 function closeFarmAnimalsThirdLayer() {
     jQuery(".FarmAnimalsThirdLayer").hide();
     jQuery(".OurWorkSecondLayer").show("slide", {direction: "right"}, slideTime);
-    ;
+
 }
 
