8d7387b7278a667e6450cd211a3f40b885142106
Nathalie Senechal <ns@move-elevator.de>
2020-03-11 12:13:23 +0100

    updated names, deleted unused file

--- shelter thingy/resources/JS/linkLength.js
+++
@@ -1,9 +0,0 @@
-
-var accordionLink = document.getElementsByClassName('AccordionLink');
-
-var width = accordionLink.offsetWidth;
-
-function mouseOver() {
-    accordionLink.setAttribute("style, background-size: 100% 2px");
-    console.log(width);
-}
