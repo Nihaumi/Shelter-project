4d272331650b972fcbca0f58c78ebc6960394289
Nathalie Senechal <ns@move-elevator.de>
2020-03-12 15:36:58 +0100

    MEINTRA-4189  added class .logo in logo.scss

--- web/online-styleguide/branding/logo/logo.php
+++ web/online-styleguide/branding/logo/logo.php
@@ -15,15 +15,7 @@
 				<a href="#" class="logo">IKK Berlin Brandenburg</a>
 			</div>
 
-			<h2 class="prototype__subline">Logo mit Signet</h2>
-			<p class="prototype__text">Dieses Logo wird in der mobilen Navigation verwendet.</p>
-			<div class="prototype__branding__view prototype__branding__view--center">
-				<a href="#" class="logo logo--signet">IKK Berlin Brandenburg</a>
-			</div>
-			<p class="prototype__block-headline">Sourcecode</p>
-			<div class="prototype__component__sourcecode">
-				<a href="#" class="logo logo--signet">IKK Berlin Brandenburg</a>
-			</div>
+
 		</div>
 		<?php include "../../includes/footer-component.php"; ?>
 	</body>
