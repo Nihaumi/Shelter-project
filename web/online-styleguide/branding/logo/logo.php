3c83d214d62ff66714dcf44036ae433b8d1a4202
Nathalie Senechal <ns@move-elevator.de>
2020-03-13 09:00:13 +0100

    MEINTRA-4189 changed logo.php (Ikkbb into Shelter)

--- web/online-styleguide/branding/logo/logo.php
+++ web/online-styleguide/branding/logo/logo.php
@@ -8,11 +8,11 @@
 			<h2 class="prototype__subline">Logo</h2>
 			<p class="prototype__text">Das ist das Standard-Logo.</p>
 			<div class="prototype__branding__view prototype__branding__view--center">
-				<a href="#" class="logo">IKK Berlin Brandenburg</a>
+				<a href="#" class="logo">Shelter</a>
 			</div>
 			<p class="prototype__block-headline">Sourcecode</p>
 			<div class="prototype__component__sourcecode">
-				<a href="#" class="logo">IKK Berlin Brandenburg</a>
+				<a href="#" class="logo">Shelter</a>
 			</div>
 
 
