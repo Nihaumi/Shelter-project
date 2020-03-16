f8e30842e4d690f17821a1de3806083a65bf5827
Nathalie Senechal <ns@move-elevator.de>
2020-03-16 14:33:10 +0100

    MEINTRA-4189  added Image.scss

--- web/online-styleguide/components/content-image/includes/wide.php
+++ web/online-styleguide/components/content-image/includes/wide.php
@@ -10,10 +10,6 @@
 	<div class="picture-container">
 
 		<picture>
-			<source srcset="//placeimg.com/450/237/any" media="(max-width:414px)"/>
-			<source srcset="//placeimg.com/780/415/any" media="(max-width:767px)"/>
-			<source srcset="//placeimg.com/1000/333/any" media="(max-width:991px)"/>
-			<source srcset="//placeimg.com/1200/300/any" media="(max-width:1199px)"/>
 			<img src="//placeimg.com/1920/640/any" alt="Lorem ipsum dolor sit amet"
 				 title="Lorem ipsum dolor sit amet">
 		</picture>
