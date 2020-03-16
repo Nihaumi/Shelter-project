cd762c74c5b756bf73d53b95856eeab72eb09798
Nathalie Senechal <ns@move-elevator.de>
2020-03-16 13:16:38 +0100

    MEINTRA-4189  deleted unnecessary (not yet used) lines

--- web/online-styleguide/components/typographies/includes/headlines-display-classes.php
+++ web/online-styleguide/components/typographies/includes/headlines-display-classes.php
@@ -9,37 +9,3 @@
 <p class="display-h1">Headline 1 (p)</p>
 
 <hr>
-
-<h1 class="display-h2">Headline 2 (h1)</h1>
-
-<h2>Headline 2 (h2)</h2>
-
-<h3 class="display-h2">Headline 2 (h3)</h3>
-
-<h4 class="display-h2">Headline 2 (h4)</h4>
-
-<p class="display-h2">Headline 2 (p)</p>
-
-<hr>
-
-<h1 class="display-h3">Headline 3 (h1)</h1>
-
-<h2 class="display-h3">Headline 3 (h2)</h2>
-
-<h3>Headline 3 (h3)</h3>
-
-<h4 class="display-h3">Headline 3 (h4)</h4>
-
-<p class="display-h3">Headline 3 (p)</p>
-
-<hr>
-
-<h1 class="display-h4">Headline 4 (h1)</h1>
-
-<h2 class="display-h4">Headline 4 (h2)</h2>
-
-<h3 class="display-h4">Headline 4 (h3)</h3>
-
-<h4>Headline 4 (h4)</h4>
-
-<p class="display-h4">Headline 4 (p)</p>
