6405f09dbbd9bc491e411a79527f5c003548b7a6
Nathalie Senechal <ns@move-elevator.de>
2020-03-17 12:27:21 +0100

    MEINTRA-4189  added scss snd webfonts folder from Fontawesome

--- web/online-styleguide/components/buttons/includes/arrows.php
+++ web/online-styleguide/components/buttons/includes/arrows.php
@@ -1,2 +1,2 @@
-<button class="btn btn--arrow">Mehr anzeigen<i class="$fa-var-arrow-right"></i></button>
-<a href="#" class="btn btn--arrow">Mehr anzeigen<i class="fa fa-arrow-right"></i></a>
+<button class="btn btn--arrow">Mehr anzeigen</button>
+<a href="#" class="btn btn--arrow">Mehr anzeigen</a>
